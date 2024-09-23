45f84524ff651b0e28d8cc81e2309df208769e51
Kunal Jindal <kunaljindal@microsoft.com>
2024-09-23 20:29:25 +0530

    Add all class codes

--- exception-handling/src/Main.java
+++ exception-handling/src/Main.java
@@ -1,28 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("A");
-        int a = 5;
-        int b = 0;
-        try{
-          int c = a/b;
-          System.out.println("D");
-        }
-        catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("C");
-        }
-        finally{
-            System.out.println("F");
+//        System.out.println("A");
+//        int a = 5;
+//        int b = 0;
+//        try{
+//          int c = a/b;
+//          System.out.println("D");
+//        }
+//        catch (ArrayIndexOutOfBoundsException e){
+//            System.out.println("C");
+//        }
+//        finally{
+//            System.out.println("F");
+//        }
+//
+//        try{
+//            int x = 5/0;
+//        }
+//        catch (ArithmeticException e) {
+//            System.out.println("G");
+//        }
+//        finally {
+//            System.out.println("H");
+//        }
+//        System.out.println("B");
+
+        int age = 10;
+            checkAge(age);
+        System.out.printf("bdjdkjsa");
         }
 
-        try{
-            int x = 5/0;
+        public static void checkAge ( int age) {
+            if (age < 18) {
+                throw new UnderAgeException("You are under 18");
+            }
         }
-        catch (ArithmeticException e) {
-            System.out.println("G");
-        }
-        finally {
-            System.out.println("H");
-        }
-        System.out.println("B");
     }
-}
